61342f9b9e5873295c6cecf60b233598ad864ad5
hxlh <49110117+hxlh@users.noreply.github.com>
2021-02-22 15:28:59 +0800

    dynamic dev

--- main.c
+++ main.c
@@ -4,38 +4,21 @@
 
 void backFunc(void *data)
 {
+    _sleep(200);
     printf("%d\n", (int)data);
 }
 int main()
 {
-    Task *task1 = (Task *)malloc(sizeof(Task));
-    task1->callback = backFunc;
-    task1->data = 545;
-
-    Task *task2 = (Task *)malloc(sizeof(Task));
-    task2->callback = backFunc;
-    task2->data = 858;
-    Task *task3 = (Task *)malloc(sizeof(Task));
-    task3->callback = backFunc;
-    task3->data = 1010;
-
-    Task *task4 = (Task *)malloc(sizeof(Task));
-    task4->callback = backFunc;
-    task4->data = 2000;
-
-    List *tasks = NULL;
-    List_add(tasks, task1->item);
-
-    List_add(tasks, task2->item);
-    List_add(tasks, task3->item);
-    List_add(tasks, task4->item);
-
-    List_del(tasks,task1->item);
-    List_del(tasks,task2->item);
-    List_del(tasks,task3->item);
-    List_del(tasks,task4->item);
-    
-    Task *ttttt = NULL;
-    List_take(tasks, Task, item, ttttt);
-    ttttt->callback(ttttt->data);
+    ThreadPool* pool=pool_getPool();
+    pool_run(pool,20);
+    for (size_t i = 1000; i < 1100; i++)
+    {
+        pool_addTask(pool,backFunc,i);
+        // _sleep(100);
+    }
+    pool_close(pool);
+    _sleep(1000);
+    printf("num: %d\n",pool->tCount);
+    printf("destroy: %d\n", pool_destroy(pool));
+    getchar();
 }
--- threadpool.h
+++ threadpool.h
@@ -2,18 +2,26 @@
 #define THREADPOOL_H
 
 #include <pthread.h>
+#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 #define List_data(list_item, type, mber_name) \
-    (type *)((char *)list_item - offsetof(type, mber_name))
+    (type *)((char *)(list_item)-offsetof(type, mber_name))
 
-#define List_take(head, type, mber_name, out)  \
-    {                                          \
-        struct pt_list *tmp = (head);          \
-        (head) = (head)->next;                 \
-        (head)->prev = NULL;                   \
-        out = List_data(tmp, type, mber_name); \
+#define List_take(head, type, mber_name, out)     \
+    if ((head) == NULL)                           \
+    {                                             \
+        out = NULL;                               \
+    }                                             \
+    else                                          \
+    {                                             \
+        out = List_data((head), type, mber_name); \
+        (head) = (head)->next;                    \
+        if (head != NULL)                         \
+        {                                         \
+            (head)->prev = NULL;                  \
+        }                                         \
     }
 
 #define List_add(head, item)    \
@@ -71,6 +79,8 @@ typedef struct ThreadPool_t
     int runCount;
     //总共创建的线程数量
     int tCount;
+    //最小线程数
+    int mintCount;
     //任务总数
     unsigned int taskCount;
     //线程池全局锁
@@ -101,19 +111,131 @@ typedef struct pt_worker
 ThreadPool *pool_getPool()
 {
     ThreadPool *pool = (ThreadPool *)malloc(sizeof(ThreadPool));
-    pool->cond = pthread_cond_init(&(pool->cond), NULL);
+    pthread_cond_init(&(pool->cond), NULL);
     pool->isClose = 0;
-    pool->locker = pthread_mutex_init(&(pool->locker), NULL);
+    pthread_mutex_init(&(pool->locker), NULL);
     pool->runCount = 0;
     pool->taskCount = 0;
     pool->tasks = NULL;
     pool->tCount = 0;
+    pool->mintCount = 0;
     pool->workers = NULL;
     return pool;
 };
 
+void pt_threadRun(void *arg)
+{
+    Worker *w = (Worker *)arg;
+    for (;;)
+    {
+        pthread_mutex_lock(&(w->pool->locker));
+        for (; w->pool->taskCount <= 0 && w->isExit<=0  ;)
+        {
+            pthread_cond_wait(&(w->pool->cond), &(w->pool->locker));
+        }
+        if (w->isExit > 0)
+        {
+            break;
+        }
+
+        w->pool->runCount++;
+        
+        Task *task = NULL;
+        List_take(w->pool->tasks, Task, item, task);
+
+        if (task == NULL)
+        {
+            w->pool->runCount--;
+            continue;
+        }
+
+        w->pool->taskCount--;
+
+        pthread_mutex_unlock(&(w->pool->locker));
+
+        task->callback(task->data);
+        free(task);
+
+        pthread_mutex_lock(&(w->pool->locker));
+        w->pool->runCount--;
+        if (w->isExit > 0)
+        {
+            break;
+        }
+        pthread_mutex_unlock(&(w->pool->locker));
+    }
+
+    w->pool->tCount--;
+
+    pthread_mutex_unlock(&w->pool->locker);
+    
+    free(w);
+    w=NULL;
+
+    printf("thread exit\n");
+}
+//1.25倍增幅
+void pt_pool_expand(ThreadPool *pool)
+{
+    int num = ceil((double)pool->tCount * 1.25);
+
+    for (size_t i = 0; i < num; i++)
+    {
+        Worker *worker = (Worker *)malloc(sizeof(Worker));
+
+        pthread_t id;
+
+        worker->id = id;
+        worker->isExit = 0;
+        worker->pool = pool;
+        List_add(pool->workers, worker->item);
+
+        pthread_create(&id, NULL, pt_threadRun, (void *)worker);
+
+        pool->tCount++;
+    }
+}
+
+void pt_pool_recycle(ThreadPool *pool)
+{
+    int diff = pool->tCount - pool->mintCount;
+    if (diff > 0)
+    {
+        for (size_t i = 0; i < diff; i++)
+        {
+            pthread_cond_signal(&(pool->cond));
+        }
+    }
+}
+
+void pool_addTask(ThreadPool *pool, void (*callback)(void *), void *data)
+{
+    pthread_mutex_lock(&pool->locker);
+    if (pool->isClose != 0)
+    {
+        pthread_mutex_unlock(&pool->locker);
+        return;
+    }
+    if (pool->runCount >= pool->tCount)
+    {
+        //增设线程
+        pt_pool_expand(pool);
+    }
+
+    Task *task = (Task *)malloc(sizeof(Task));
+    task->callback = callback;
+    task->data = data;
+
+    List_add(pool->tasks, task->item);
+    pool->taskCount++;
+    pthread_cond_signal(&pool->cond);
+
+    pthread_mutex_unlock(&pool->locker);
+};
+
 void pool_run(ThreadPool *pool, int threadCount)
 {
+
     for (size_t i = 0; i < threadCount; i++)
     {
         Worker *worker = (Worker *)malloc(sizeof(Worker));
@@ -126,80 +248,15 @@ void pool_run(ThreadPool *pool, int threadCount)
         List_add(pool->workers, worker->item);
         pthread_create(&id, NULL, pt_threadRun, (void *)worker);
         pool->tCount++;
+        pool->mintCount++;
     }
 }
-
-void pt_threadRun(void *arg)
-{
-    Worker *w = (Worker *)arg;
-    for (;;)
-    {
-        pthread_mutex_lock(&(w->pool->locker));
-        for (; w->pool->taskCount > 0;)
-        {
-            pthread_cond_wait(&(w->pool->cond), &(w->pool->locker));
-        }
-        pthread_mutex_unlock(&(w->pool->locker));
-
-        if (w->isExit!=0)
-        {
-            break;
-        }
-        
-        pthread_mutex_lock(&(w->pool->locker));
-
-        w->pool->runCount++;
-        Task *task = NULL;
-        List_take(w->pool->tasks, Task, item, task);
-        w->pool->taskCount--;
-
-        pthread_mutex_unlock(&(w->pool->locker));
-
-        task->callback(task->data);
-        free(task);
-
-        pthread_mutex_lock(&(w->pool->locker));
-        w->pool->runCount--;
-        pthread_mutex_unlock(&(w->pool->locker));
-
-
-        if (w->isExit!=0)
-        {
-            break;
-        }
-    }
-    pthread_mutex_lock(w->pool->locker);
-
-    w->pool->tCount--;
-    List_del(w->pool->workers, w->item);
-    free(w);
-
-    pthread_mutex_unlock(w->pool->locker);
-}
-
-void pool_addTask(ThreadPool *pool, void (*callback)(void *), void *data)
-{
-    if (pool->isClose != 0)
-    {
-        return;
-    }
-    Task *task = (Task *)malloc(sizeof(Task));
-    task->callback = callback;
-    task->data = data;
-
-    pthread_mutex_lock(&pool->locker);
-
-    List_add(pool->tasks, task->item);
-    pool->taskCount++;
-
-    pthread_mutex_unlock(&pool->locker);
-
-    pthread_cond_signal(&pool->cond);
-};
 
 //先close，后destroy
 void pool_close(ThreadPool *pool)
 {
+    pthread_mutex_lock(&(pool->locker));
+
     pool->isClose = 1;
     for (size_t i = 0; i < pool->tCount; i++)
     {
@@ -207,24 +264,20 @@ void pool_close(ThreadPool *pool)
         List_take(pool->workers, Worker, item, temp);
         temp->isExit = 1;
     }
-    pthread_cond_broadcast(pool->cond);
+    pthread_cond_broadcast(&(pool->cond));
+    pthread_mutex_unlock(&(pool->locker));
+    
 }
 
-//返回值 ：0为销毁失败
-int pool_destroy(ThreadPool* pool){
-    if (pool->isClose==0)
-    {
-        return 0;
-    }
-    if (pool->tCount>0)
-    {
-        pthread_cond_broadcast(pool->cond);
-    }
-    if (pool->tCount<=0)
+//返回值 ：0为销毁失败,注意请close后延时一段时间才调用，close后线程池先完成任务才退出
+int pool_destroy(ThreadPool *pool)
+{
+    if (pool->tCount <= 0)
     {
         pthread_cond_destroy(&pool->cond);
         pthread_mutex_destroy(&pool->locker);
         free(pool);
+        pool=NULL;
         return 1;
     }
     return 0;
